6af1efd0ee6809fc3bb1a68262ec90e69cfe0e55
HandaIBAGZ <91827363+HandaIBAGZ@users.noreply.github.com>
2026-05-15 15:28:47 -0700

    Implemented findContact and viewContact methods

--- ContactBookManager.java
+++ ContactBookManager.java
@@ -1,3 +1,51 @@
+
 public class ContactBookManager {
-    
+    private Contact[] contacts = new Contact[100];
+
+    public int findContactbyName(String name) {
+        for (int i = 0; i < contactCount; i++) {
+            if (contacts[i].getName().equalsIgnoreCase(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findContactbyEmail(String email) {
+        for (int i = 0; i < contactCount; i++) {
+            if (contacts[i].getEmail().equalsIgnoreCase(email)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findContactbyPhoneNumber(String phoneNumber) {
+        for (int i = 0; i < contactCount; i++) {
+            if (contacts[i].getPhoneNumber().equalsIgnoreCase(phoneNumber)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void viewContact(int contactIndex) {
+        if (contactIndex >= 0 && contactIndex < contactCount) {
+            System.out.println("Contact Details:");
+
+            System.out.println(contacts[contactIndex]);
+        } else {
+            System.out.println("Invalid contact index.");
+        }
+    }
+
+    public void viewAllContacts() {
+        System.out.println("All Contact Details:");
+
+        for (int i = 0; i < contactCount; i++) {
+            System.out.println("Contact " + (i + 1) + ":");
+            System.out.println(contacts[i]);
+        }
+    }
+
 }
